c6af6c7c9aa4fe9078ff1c3a7b0e66f8590e8d37
noteden <migdalekj@op.pl>
2019-06-04 07:44:05 +0200

    Gametextv1

---
+++ Migdałek/Assets/GraSc.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraSc : MonoBehaviour {
+
+    public int wybor = 0;
+    public GameObject[] obiekty;
+
+	void Update () {
+        for (int i = 0; i < obiekty.Length; i++)
+        {
+            if (i == wybor)
+            {
+                obiekty[i].gameObject.SetActive(true);
+            }
+            else {
+                obiekty[i].gameObject.SetActive(false);
+            }
+        }
+	}
+
+    public void DokonanieWyboru(int i)
+    {
+        wybor = i;
+    }
+}
